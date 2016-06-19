68bd7f8dd89c614e90c04b493675217400053f10
Agustin K-ballo Berge <k@fusionfenix.com>
2016-06-19 17:07:00 -0300

    Add C++11 range utilities

--- hpx/lcos/dataflow.hpp
+++ hpx/lcos/dataflow.hpp
@@ -76,7 +76,7 @@ namespace hpx { namespace lcos { namespace detail
         template <typename Future>
         HPX_FORCEINLINE
         typename std::enable_if<
-            traits::is_future_or_future_range<Future>::value
+            traits::detail::is_future_or_future_range<Future>::value
         >::type operator()(Future& future) const
         {
             future = Future();
@@ -85,7 +85,7 @@ namespace hpx { namespace lcos { namespace detail
         template <typename Future>
         HPX_FORCEINLINE
         typename std::enable_if<
-            traits::is_future_or_future_range<Future>::value
+            traits::detail::is_future_or_future_range<Future>::value
         >::type operator()(boost::reference_wrapper<Future>& future) const
         {
             future.get() = Future();
@@ -94,7 +94,7 @@ namespace hpx { namespace lcos { namespace detail
         template <typename Future>
         HPX_FORCEINLINE
         typename std::enable_if<
-            traits::is_future_or_future_range<Future>::value
+            traits::detail::is_future_or_future_range<Future>::value
         >::type operator()(std::reference_wrapper<Future>& future) const
         {
             future.get() = Future();
@@ -103,7 +103,7 @@ namespace hpx { namespace lcos { namespace detail
         template <typename Future>
         HPX_FORCEINLINE
         typename std::enable_if<
-            !traits::is_future_or_future_range<Future>::value
+            !traits::detail::is_future_or_future_range<Future>::value
         >::type operator()(Future& future) const
         {}
     };
